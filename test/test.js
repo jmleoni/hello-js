ff6bc513b77fcae138b09074ff4e46e8e364779d
Jean-Marc Leoni <jeanmarc.leoni@axa.com>
2018-03-08 10:51:05 +0100

    adding tests

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+
+describe('Hello', function() {
+  it('should say hello on / GET', function(done) {
+  chai.request(server)
+    .get('/')
+    .end(function(err, res){
+      res.should.have.status(200);
+      done();
+    });
+  });
+});
